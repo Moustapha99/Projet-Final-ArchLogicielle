ca69d43cf30cd4de435d6275443d8dd42b13e0a8
Moustapha Adrien <moustaphaadrien@gmail.com>
2022-07-24 21:00:44 +0000

    api 1.0
    récupération des données OK
    reste à trier par catégories et par un seul article

--- API rest/index.js
+++ API rest/index.js
@@ -1,33 +1,55 @@
 const express = require('express');
-const express = require('express');
-const mysql = require('mysql');
+// const mysql = require('mysql');
 const app = express();
+const port = 5000;
+const articleRouter = require('./routes/articles');
+const categoryRouter = require('./routes/articles/id');
 
 app.use(express.json());
-app.get("/", (req, res) => {  // req = request, res = response
-    const article = req.query.article;  
-    res.send(article + "!");   
-});         
+app.use(
+    express.urlencoded({
+        extended: true
+    })
+);
 
 
-        //post request
-const article = [];
-app.post("/article", (req, res) => {
-    const { article }= req.body;
+        
+        /*  ------------------- definition de la route ------------------- */
+        
+// app.get("/", (req, res) => {  // req = request, res = response   
+//     const article = req.query.article;  
+//     res.send(article + "!");   
+// });         
+app.use("/articles", articleRouter);
+app.use("articles/id", categoryRouter);
 
-    user.push({ article: article.titre, id: article.id});
+        /* ------------------- middleware qui gére les erreurs ------------------- */
+    app.use((err, req, res, next) => {
+        const statusCode = err.statusCode || 500;
+        console.error(err.message, err.stack);
+        res.status(statusCode).json({ message: err.message});
+        return;
+    } );
 
-    res.json({ article: article.titre, id: article.id});
-});
+    app.listen(port, () => {
+        console.log('Server started on port 5000');
+    });
 
-app.get("/article", async (req, res) => {
-    const query = "SELECT * FROM articles";
-    createPool.
+//         //post request
+// const article = [];
+// app.post("/article", (req, res) => {
+//     const { article }= req.body;
+
+//     user.push({ article: article.titre, id: article.id});
+
+//     res.json({ article: article.titre, id: article.id});
+// });
+
+// app.get("/article", async (req, res) => {
+//     const query = "SELECT * FROM articles";
+//     createPool.
 
 
 
 
-app.listen(5000, () => {
-    console.log('Server started on port 5000');
-});
 
--- mglsi_news.sql
+++ mglsi_news.sql
@@ -1,147 +1,53 @@
--- phpMyAdmin SQL Dump
--- version 5.0.4
--- https://www.phpmyadmin.net/
---
--- Hôte : localhost
--- Généré le : sam. 23 juil. 2022 à 17:27
--- Version du serveur :  10.4.17-MariaDB
--- Version de PHP : 8.0.0
+CREATE DATABASE IF NOT EXISTS mglsi_news;
+USE mglsi_news;
 
-SET SQL_MODE = "NO_AUTO_VALUE_ON_ZERO";
-START TRANSACTION;
-SET time_zone = "+00:00";
+-- DROP TABLE Article, Categorie, Users, Roles;
 
 
-/*!40101 SET @OLD_CHARACTER_SET_CLIENT=@@CHARACTER_SET_CLIENT */;
-/*!40101 SET @OLD_CHARACTER_SET_RESULTS=@@CHARACTER_SET_RESULTS */;
-/*!40101 SET @OLD_COLLATION_CONNECTION=@@COLLATION_CONNECTION */;
-/*!40101 SET NAMES utf8mb4 */;
+CREATE TABLE Article(
+	id int primary key auto_increment,
+	titre varchar(255),
+	contenu text,
+	dateCreation datetime DEFAULT NOW(),
+	dateModification datetime DEFAULT NOW(),
+	categorie int
+);
 
---
--- Base de données : `mglsi_news`
---
 
--- --------------------------------------------------------
+CREATE TABLE Categorie(
+	id int primary key auto_increment,
+	libelle varchar(20)
+);
 
---
--- Structure de la table `Article`
---
-
-CREATE TABLE Article (
-  `id` int(11) NOT NULL,
-  `titre` varchar(255) DEFAULT NULL,
-  `contenu` text DEFAULT NULL,
-  `dateCreation` datetime DEFAULT current_timestamp(),
-  `dateModification` datetime DEFAULT current_timestamp(),
-  `categorie` int(11) DEFAULT NULL
-) 
-
---
--- Déchargement des données de la table `Article`
---
-
-INSERT INTO Article (id, titre, contenu, dateCreation, dateModification, categorie) VALUES
-(1, 'Première victoire du Sénégal', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2022-07-20 23:24:38', '2022-07-20 23:24:38', 1),
-(2, 'Election en Mauritanie', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2022-07-20 23:24:38', '2022-07-20 23:24:38', 4),
-(3, 'Début de la CAN', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2022-07-20 23:24:38', '2022-07-20 23:24:38', 1),
-(4, 'Pétrole au Sénégal', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2022-07-20 23:24:38', '2022-07-20 23:24:38', 4),
-(5, 'Inauguration d\'un ENO à l\'UVS', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2022-07-20 23:24:38', '2022-07-20 23:24:38', 3),
-(6, 'Don de sang: campus social', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2022-07-20 23:24:39', '2022-07-20 23:24:39', 2),
-(7, 'Notre réponse à la Covid-19', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2022-07-20 23:24:39', '2022-07-20 23:24:39', 2),
-(8, 'Salon du Polytechnicien', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', '2022-07-20 23:24:39', '2022-07-20 23:24:39', 3);
-
--- --------------------------------------------------------
-
---
--- Structure de la table `Categorie`
---
-
-CREATE TABLE Categorie (
-  id int(11) NOT NULL,
-  libelle varchar(20) DEFAULT NULL
-)
-
---
--- Déchargement des données de la table `Categorie`
---
-
-INSERT INTO Categorie (id, libelle) VALUES
-(1, 'Sport'),
-(2, 'Santé'),
-(3, 'Education'),
-(4, 'Politique');
-
--- --------------------------------------------------------
-
---
--- Structure de la table `Users`
---
 
 CREATE TABLE Users (
-    id int NOT NULL primary key,
+    id int NOT NULL primary key auto_increment,
     nom varchar(50) NOT NULL,
     prenom varchar(50) NOT NULL,
+	roles int,
     login varchar(50) NOT NULL,
     password varchar(50) NOT NULL
-    
-) --ENGINE=InnoDB DEFAULT CHARSET=utf8mb4;
+);
 
---
--- Index pour les tables déchargées
---
+CREATE TABLE Roles (
+	id int NOT NULL primary key auto_increment,
+	libelle varchar(20) NOT NULL
+);
 
---
--- Index pour la table `Article`
---
-ALTER TABLE Article
-  ADD PRIMARY KEY (id),
-  ADD KEY fk_categorie_article (categorie);
+ALTER TABLE Article ADD CONSTRAINT fk_categorie_article FOREIGN KEY(categorie) REFERENCES Categorie(id);
+ALTER TABLE Users ADD CONSTRAINT fk_roles_users FOREIGN KEY(roles) REFERENCES Roles(id);
 
---
--- Index pour la table `Categorie`
---
-ALTER TABLE Categorie
-  ADD PRIMARY KEY (id);
+INSERT INTO Roles (libelle) VALUES ('Administrateur'), ('Editeur'), ('Visiteur');
 
---
--- Index pour la table `Utilisateurs`
---
--- ALTER TABLE `Users`
---   ADD PRIMARY KEY (`id`);
+INSERT INTO Categorie(libelle) VALUES ('Sport'), ('Santé'), ('Education'), ('Politique');
 
---
--- AUTO_INCREMENT pour les tables déchargées
---
+INSERT INTO Article (titre, contenu, categorie) VALUES ('Première victoire du Sénégal', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 1),
+	('Election en Mauritanie', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 4),
+	('Début de la CAN', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 1),
+	('Pétrole au Sénégal', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 4),
+	("Inauguration d'un ENO à l'UVS", 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 3),
+	('Don de sang: campus social', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 2),
+	('Notre réponse à la Covid-19', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 2),
+	('Salon du Polytechnicien', 'Lorem ipsum dolor sit amet, consectetur adipisicing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.', 3);
 
---
--- AUTO_INCREMENT pour la table `Article`
---
-ALTER TABLE Article
-  MODIFY `id` int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `Categorie`
---
-ALTER TABLE Categorie,
-  MODIFY id int(11) NOT NULL AUTO_INCREMENT;
-
---
--- AUTO_INCREMENT pour la table `Users`
---
--- ALTER TABLE `Utilisateurs`
---   MODIFY `id` bigint(20) NOT NULL AUTO_INCREMENT;
-
---
--- Contraintes pour les tables déchargées
---
-
---
--- Contraintes pour la table `Article`
---
-ALTER TABLE Article
-  ADD CONSTRAINT `fk_categorie_article` FOREIGN KEY (`categorie`) REFERENCES `Categorie` (`id`);
-COMMIT;
-
-/*!40101 SET CHARACTER_SET_CLIENT=@OLD_CHARACTER_SET_CLIENT */;
-/*!40101 SET CHARACTER_SET_RESULTS=@OLD_CHARACTER_SET_RESULTS */;
-/*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
+-- GRANT ALL PRIVILEGES ON mglsi_news.* TO mglsi_user IDENTIFIED BY 'passer';
